a9338becafa14fba10a3bf01acf2c02f0252ce86
superevilstockholm <balilombok11@gmail.com>
2026-04-21 15:45:58 +0700

    Created sidebar helper

---
+++ app/Helpers/SidebarHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!function_exists('adminSidebarItems')) {
+    function adminSidebarItems(): array
+    {
+        return [
+            'main' => [
+                ['label' => 'dashboard', 'icon' => 'ti ti-dashboard', 'route' => 'dashboard.admin.index', 'active_pattern' => 'dashboard.admin.index'],
+            ],
+        ];
+    }
+}
+
+if (!function_exists('teacherSidebarItems')) {
+    function teacherSidebarItems(): array
+    {
+        return [
+            'main' => [
+                ['label' => 'dashboard', 'icon' => 'ti ti-dashboard', 'route' => 'dashboard.teacher.index', 'active_pattern' => 'dashboard.teacher.index'],
+            ],
+        ];
+    }
+}
