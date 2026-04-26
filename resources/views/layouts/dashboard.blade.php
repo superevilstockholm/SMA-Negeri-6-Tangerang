7919b42ba3d65d880a244791e71652c85fce87fb
superevilstockholm <balilombok11@gmail.com>
2026-04-26 15:08:38 +0700

    Add breadcrumb to dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,6 +4,38 @@
     <x-topbar></x-topbar>
     <div class="pc-container">
         <div class="pc-content">
+            <div class="row mb-4">
+                <div class="col">
+                    @php
+                        $segments = request()->segments();
+                        $role = $segments[1] ?? null;
+                        $group = $segments[2] ?? null;
+                        $page = $segments[3] ?? null;
+                        $dashboardRoute = "dashboard.$role.index";
+                        $pageRoute = $page ? "dashboard.$role.$group.$page.index" : null;
+                    @endphp
+                    <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                            {{-- Dashboard --}}
+                            <li class="breadcrumb-item">
+                                <a href="{{ route($dashboardRoute) }}">Dashboard</a>
+                            </li>
+                            {{-- Group --}}
+                            @if($group)
+                                <li class="breadcrumb-item">
+                                    {{ ucwords(str_replace('-', ' ', $group)) }}
+                                </li>
+                            @endif
+                            {{-- Page --}}
+                            @if($page)
+                                <li class="breadcrumb-item active">
+                                    {{ ucwords(str_replace('-', ' ', $page)) }}
+                                </li>
+                            @endif
+                        </ol>
+                    </nav>
+                </div>
+            </div>
             @yield('content')
         </div>
     </div>
