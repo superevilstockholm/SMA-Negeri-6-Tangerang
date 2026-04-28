09a3c4a3d8488c2cd2e730250c23e80b41bee5e6
superevilstockholm <balilombok11@gmail.com>
2026-04-28 15:46:43 +0700

    Updated dashboard sidebar icons

--- app/Helpers/SidebarHelper.php
+++ app/Helpers/SidebarHelper.php
@@ -8,10 +8,10 @@ if (!function_exists('adminSidebarItems')) {
                 ['label' => 'dashboard', 'icon' => 'ti ti-dashboard', 'route' => 'dashboard.admin.index', 'activePattern' => 'dashboard.admin.index'],
             ],
             'master data' => [
-                ['label' => 'visions', 'icon' => 'ti ti-eye', 'route' => 'dashboard.admin.master-data.visions.index', 'activePattern' => 'dashboard.admin.master-data.visions.*'],
-                ['label' => 'mission', 'icon' => 'ti ti-rocket', 'route' => 'dashboard.admin.master-data.missions.index', 'activePattern' => 'dashboard.admin.master-data.missions.*'],
-                ['label' => 'school histories', 'icon' => 'ti ti-book', 'route' => 'dashboard.admin.master-data.school-histories.index', 'activePattern' => 'dashboard.admin.master-data.school-histories.*'],
-                ['label' => 'contacts', 'icon' => 'ti ti-message', 'route' => 'dashboard.admin.master-data.contacts.index', 'activePattern' => 'dashboard.admin.master-data.contacts.*'],
+                ['label' => 'visions', 'icon' => 'ti ti-target', 'route' => 'dashboard.admin.master-data.visions.index', 'activePattern' => 'dashboard.admin.master-data.visions.*'],
+                ['label' => 'mission', 'icon' => 'ti ti-list-check', 'route' => 'dashboard.admin.master-data.missions.index', 'activePattern' => 'dashboard.admin.master-data.missions.*'],
+                ['label' => 'school histories', 'icon' => 'ti ti-timeline-event', 'route' => 'dashboard.admin.master-data.school-histories.index', 'activePattern' => 'dashboard.admin.master-data.school-histories.*'],
+                ['label' => 'contacts', 'icon' => 'ti ti-message-2-share', 'route' => 'dashboard.admin.master-data.contacts.index', 'activePattern' => 'dashboard.admin.master-data.contacts.*'],
             ]
         ];
     }
