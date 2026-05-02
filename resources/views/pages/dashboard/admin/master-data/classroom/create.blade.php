96c3c37ca3464aed7597a4b3230c819780eb0166
superevilstockholm <balilombok11@gmail.com>
2026-05-02 20:38:03 +0700

    Created create classroom page

---
+++ resources/views/pages/dashboard/admin/master-data/classroom/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.dashboard')
+@section('title', 'Create Classroom')
+@section('content')
+    <div class="row mb-4">
+        <div class="col">
+            <div class="card my-0">
+                <div
+                    class="card-body d-flex flex-column flex-md-row align-items-md-center justify-content-md-between gap-2 gap-lg-5">
+                    <div class="d-flex flex-column">
+                        <h3 class="p-0 m-0 mb-1 fw-semibold">Create Classroom</h3>
+                        <p class="p-0 m-0 fw-medium text-muted">Create a new classroom.</p>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <a href="{{ route('dashboard.admin.master-data.classrooms.index') }}"
+                            class="btn btn-sm btn-primary d-flex align-items-center gap-2 justify-content-center px-4 rounded-pill m-0">
+                            <i class="ti ti-arrow-left me-1"></i> Back to List
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card my-0">
+                <div class="card-body">
+                    <form action="{{ route('dashboard.admin.master-data.classrooms.store') }}" autocomplete="off"
+                        method="POST">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="floatingInputName"
+                                placeholder="Name" autocomplete="off" value="{{ old('name') }}" autofocus>
+                            <label for="floatingInputName">Name <span class="text-danger">*</span></label>
+                            @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary d-flex align-items-center gap-2 justify-content-center px-4 rounded-pill">
+                                <i class="ti ti-device-floppy me-1"></i> Save Classroom
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
